5c1d6576d0151c187e385537335dd737301e988f
saikiranreddyappidi <saikiranreddyvfstr@gmail.com>
2022-09-27 21:41:20 +0530

    Few bugs fixed.

--- LinkedList.c
+++ LinkedList.c
@@ -20,47 +20,58 @@ void print(struct node *start){
     }
 }
 
-int insert(struct node *past,int value){
-    struct node *future=past;
-    while(future->next != NULL){
+int insert(struct node *start,int value){
+    struct node *future=start;
+    while(future != NULL){
         future=future->next;
-        if(past->data <=value && future->data > value){
+        // if(start->data >value){
+        //     printf("---%d---",value);
+        //     struct node *new=newnode();
+        //     new->next=start->next;
+        //     new->data=value;
+        //     start->next=new;
+        //     break;
+        // }
+        if(start->data <=value && future->data > value){
             struct node *new=newnode();
-            new->next=past->next;
+            new->next=start->next;
             new->data=value;
-            past->next=new;
+            start->next=new;
             break;
         }
         else if(future->next == NULL){
             struct node *new=newnode();
             new->data=value;
             future->next=new;
-            new->next=NULL;
+            future->data=value;
             break;
         }
-        past=past->next;
+        start=start->next;
     }
 }
 
 void main(){
     struct node *start=newnode();
-    struct node *ptr1=newnode();
-    start->next=ptr1;
-    int i,n=5,t;
-    printf("Enter 5 numbers: ");
+    struct node *present=newnode();
+    start->next=present;
+    int i,n,value;
+    printf("Enter the size of linked list to be created initially: ");
+    scanf("%d",&n);
+    printf("Enter %d numbers: ",n);
     for(i=0;i<n;i++){
-        scanf("%d",&t);
-        struct node *present=newnode();
-        ptr1->next=present;
-        ptr1->data=t;
-        ptr1=present;
+        scanf("%d",&value);
+        present->data=value;
+        present->next=newnode();
+        present=present->next;
     }
     printf("\nEntered numbers : ");
     print(start);
-    printf("\nEnter a number that are to be inserted in the above list: ");
-    for(i=0;i<1;i++){
-        scanf("%d",&t);
-        insert(start,t);
+    printf("\nEnter the no.of numbers to be inserted in above the above linked list: ");
+    scanf("%d",&n);
+    printf("\nEnter %d numbers that are to be inserted in the linked list: ",n);
+    for(i=0;i<n;i++){
+        scanf("%d",&value);
+        insert(start,value);
     }
     printf("\nFinal list: ");		
     print(start);

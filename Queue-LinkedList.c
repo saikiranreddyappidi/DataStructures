0e6ad172ab22a3b865e4f0b7d4756da102f5524b
Sai Kiran Reddy Appidi <98457635+saikiranreddyappidi@users.noreply.github.com>
2022-10-13 15:45:23 +0530

    Update Queue-LinkedList.c

--- Queue-LinkedList.c
+++ Queue-LinkedList.c
@@ -19,20 +19,30 @@ struct Queue *Enqueue(struct Queue *rear){
     return rear->next;
 }
 struct Queue *Dequeue(struct Queue *front){
-    if(front->data!=-1){
+	if(front->data==-2){
+        front=front->next;
+    }
+    if(front->data==-1){
         printf("Queue is empty.");
         return front;
     }
     else{
-        return front->next;
+    	printf("%d is deleted from the Queue.",front->data);
+    	//printf("---%d---",front->next->data);
+//    	front=front->next;
+        return front;
     }
 }
 int display(struct Queue *front){
-    if(front->data!=-1){
+//    if(front->data==-2){
+//        front=front->next;
+//    }
+    if(front->data==-1){
         printf("Queue is empty.");
     }
     else{
         front=front->next;
+        printf("\nThe following elements are in the Queue: ");
         while(front->next !=NULL){
             printf("%d ",front->data);
             front=front->next;
@@ -46,14 +56,19 @@ int main(){
     front->data=-1;
     struct Queue *rear=newnode();
     front->next=rear;
-    rear->data=-1;
     while(choice!=0){
         printf("\n1-Enqueue\t2-Dequeue\t3-Display\t0-Exit\nEnter your choice: ");
         scanf("%d",&choice);
-        if(choice==1)
-            rear=Enqueue(rear);
-        else if(choice==2)
-            front=Dequeue(front);
+        if(choice==1){
+        	if(front->data==-1){
+        		front->data=-2;
+			}
+			rear=Enqueue(rear);
+		}
+        else if(choice==2){
+        	front=Dequeue(front);
+        	//printf("===%d===",front->data);
+		}
         else if(choice==3)
             display(front);
         else if(choice==0)

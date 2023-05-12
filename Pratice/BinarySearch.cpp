77c75d4585015fba1ade8011c158b862153c7351
saikiranreddyappidi <saikiranreddyvfstr@gmail.com>
2023-05-12 09:29:36 +0530

    updated

--- Pratice/BinarySearch.cpp
+++ Pratice/BinarySearch.cpp
@@ -2,18 +2,25 @@
 using namespace std;
 
 int main(){
-    int arr[]={2,4,5,7,11,15,18,100},key=11;
-    int n=sizeof(arr)/sizeof(arr[0]);
-    int low=0,high=n,mid=(low+high)/2;
+    int n,key;
+    cin>>n>>key;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    int low=0,high=n-1,mid=(low+high)/2;
     while(low<=high){
         if(arr[mid]==key){
             cout<<"Element found at: "<<mid;
             break;
         }
-        else if(arr[mid]<key)
+        else if(arr[mid]<key){
+            cout<<"Element not found in left half of array";
             low=mid+1;
-        else
+        }
+        else{
+            cout<<"Element not found in right half of array";
             high=mid-1;
+        }
         mid=(low+high)/2;        
     }
 }
--- Pratice/main.c
+++ Pratice/main.c
@@ -1,5 +1,78 @@
-#include<stdio.h>
-int main(){
-  printf("Hello World !!!");
-  return 0;
+#include <stdio.h>
+#include <limits.h>
+
+#define MAX_N 100
+#define MAX_L 1000
+
+int C[MAX_N+1][MAX_L+1];
+int cuts[MAX_N+1];
+
+
+int solve(int n, int L) {
+    int i, j, k, cost;
+
+    for (i = 1; i <= n; i++) {
+        cuts[i] = 0; /* initialize cuts array */
+        C[i][0] = 0; /* base case */
+    }
+    for (j = 1; j <= L; j++) {
+        C[1][j] = j; /* base case */
+    }
+    for (i = 2; i <= n; i++) {
+        for (j = 1; j <= L; j++) {
+            C[i][j] = INT_MAX;
+            for (k = 1; k <= j; k++) {
+                cost = k + C[i-1][j-k];
+                if (cost < C[i][j]) {
+                    C[i][j] = cost;
+                    cuts[i] = k; /* update cuts array */
+                }
+            }
+        }
+    }
+    return C[n][L];
+}
+
+void print_cuts(int n, int L, int *pieces) {
+    int i, cut, remaining;
+
+    printf("To cut a rod of length %d into %d pieces of lengths: ", L, n);
+    for (i = 0; i < n; i++) {
+        printf("%d ", pieces[i]);
+    }
+    printf("\n");
+
+    printf("Make the following cuts to achieve the minimum cost of %d:\n", C[n][L]);
+    for (i = n; i >= 1; i--) {
+        cut = cuts[i];
+        remaining = L - cut;
+        printf("Cut %d: Length = %d, Cost = %d\n", n-i+1, cut, cut + C[i-1][remaining]);
+        L = remaining; /* update remaining length */
+    }
+}
+
+int main() {
+    int n, L, i;
+    int pieces[MAX_N];
+    printf("Enter the length of the rod: ");
+    scanf("%d", &L);
+
+    printf("Enter the number of pieces: ");
+    scanf("%d", &n);
+
+    printf("Enter the lengths of the pieces:\n");
+    for (i = 0; i < n; i++) {
+        scanf("%d", &pieces[i]);
+    }
+
+    int cost = solve(n, L),max=0;
+    for(i=0;i<n;i++){
+        if(pieces[i]>max){
+            max=pieces[i];
+        }
+    }
+    printf("\nMinimum cost: %d\n", cost-max);
+    print_cuts(n, L, pieces);
+
+    return 0;
 }
